688586d4bdb2524bece560a614728b912df6c817
elite-robz <rshirley67@gmail.com>
2021-06-04 11:22:25 -0600

    added some more pages

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 console.log('hello there, obiwan');
-console.log('intersting');
-console.log('wait, how?!?!')
+
+
+(function init(){
+let page = document.getElementById('content');
+page.classList.add('header');
+
+const restaurantName = document.createElement('h1');
+restaurantName.classList.add('rest-name');
+restaurantName.textContent = 'Smoothies and Stuff'
+page.appendChild(restaurantName);
+
+})();
