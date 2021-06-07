8de90bdd4a16f4b45d6df3baefa1f40ef0c9e9eb
elite-robz <rshirley67@gmail.com>
2021-06-07 10:58:48 -0600

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-console.log('hello there, obiwan');
+console.log('hello there, Obiwan');
 
+(function init() {
+  let page = document.getElementById('content');
+  page.classList.add('header');
 
-(function init(){
-let page = document.getElementById('content');
-page.classList.add('header');
+  const restaurantName = document.createElement('h1');
+  restaurantName.classList.add('rest-name');
+  restaurantName.textContent = 'Smoothies and Stuff';
+  page.appendChild(restaurantName);
 
-const restaurantName = document.createElement('h1');
-restaurantName.classList.add('rest-name');
-restaurantName.textContent = 'Smoothies and Stuff'
-page.appendChild(restaurantName);
-
+  const selectMenu = document.createElement('h2');
+  selectMenu.classList.add('buttonMenu');
+  selectMenu.textContent = 'Home' + 'Food' + 'Smoothies' + 'Contact us';
+  page.appendChild(selectMenu);
 })();
