359ac1d362f50d8415ddf9b912495c78b4f7a410
elite-robz <rshirley67@gmail.com>
2021-06-08 15:01:14 -0600

    figured out how to add images with js

--- src/home.js
+++ src/home.js
@@ -6,6 +6,13 @@ function home(that) {
   homeCard1.textContent += 'We offer great smoothies made from organic organisms.\r\n\r\n\r\n\r\n';
   homeCard1.textContent += "We're organic and proud of it.";
   that.pageContent.appendChild(homeCard1);
+  let pic1 = document.createElement('img');
+  pic1.src = "images/360_F_223081950_xpv1HmEJAyxxhWFXS4d0djt33fXyUzhH.jpg";
+  pic1.setAttribute("height", "768");
+  pic1.setAttribute("width", "1024");
+  pic1.setAttribute("alt", "test");
+  that.pageContent.appendChild(pic1);
   }
 
   export default home;
+  
